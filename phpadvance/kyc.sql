14de03f3d88ba6a08fc2569b1827f6d155d89d9d
akratikushwaha1997 <akrati.kushwaha@diaspark.com>
2019-08-30 14:38:09 +0530

    sql added

---
+++ phpadvance/kyc.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 30, 2019 at 10:57 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kyc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+-- Error reading structure for table kyc.admin: #1932 - Table 'kyc.admin' doesn't exist in engine
+-- Error reading data for table kyc.admin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kyc`.`admin`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log`
+--
+-- Error reading structure for table kyc.log: #1932 - Table 'kyc.log' doesn't exist in engine
+-- Error reading data for table kyc.log: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kyc`.`log`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `newadmin`
+--
+-- Error reading structure for table kyc.newadmin: #1932 - Table 'kyc.newadmin' doesn't exist in engine
+-- Error reading data for table kyc.newadmin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kyc`.`newadmin`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reg`
+--
+-- Error reading structure for table kyc.reg: #1932 - Table 'kyc.reg' doesn't exist in engine
+-- Error reading data for table kyc.reg: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kyc`.`reg`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `web`
+--
+-- Error reading structure for table kyc.web: #1932 - Table 'kyc.web' doesn't exist in engine
+-- Error reading data for table kyc.web: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kyc`.`web`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
